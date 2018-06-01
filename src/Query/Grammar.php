f152cc421eeb4dc2c2341ed4c3a25e947137b94f
arthur <arth.arth247@gmail.com>
2018-06-01 15:17:40 +0400

    Hard coding guid for filter

--- src/Query/Grammar.php
+++ src/Query/Grammar.php
@@ -259,7 +259,7 @@ class Grammar implements IGrammar
     protected function whereBasic(Builder $query, $where)
     {
         //$value = $this->parameter($where['value']);
-        $value = "'".$where['value']."'";
+        $value = "guid'".$where['value']."'";
 
         return $where['column'].' '.$this->getOperatorMapping($where['operator']).' '.$value;
     }
